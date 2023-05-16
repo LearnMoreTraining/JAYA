5cc0847a628208287931848444eb4e947871d848
LearnMoreTraining <108234734+LearnMoreTraining@users.noreply.github.com>
2023-05-16 09:55:58 +0530

    ExtendReports

--- JayaTraining/src/test/java/runner/Runner.java
+++ JayaTraining/src/test/java/runner/Runner.java
@@ -13,8 +13,7 @@ import org.junit.runner.RunWith;
         glue = {"stepdefination"},
         tags = "@Table",
         plugin = {"pretty","html:target/test-output/test.html",
-                "json:target/json-output/test.json",
-                "junit:target/junit-output/test.xml"
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         }
 )
 
